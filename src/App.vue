d6ca03668f04cd33b9d398115a27a457e3101207
Simon Hryszko <simon@hryszko.dev>
2021-12-27 11:40:36 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+	import Welcome from "./components/Welcome.vue";
+</script>
+
+<template>
+  <main class="container">
+		<Welcome msg="Hello in time is ticking game!" />
+	</main>
+</template>
+
+<style>
+	@import url(https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css);
+	@import url(https://unpkg.com/@picocss/pico@latest/css/pico.min.css);
+</style>
---
+++ src/components/Welcome.vue
@@ -0,0 +1,21 @@
+<script setup>
+	import { ref } from "vue";
+
+	defineProps({
+		msg: String,
+	});
+</script>
+
+<template>
+  <center>
+		<h1 class="mt-4">
+			{{ msg }}
+
+			<button class="secondary mt-4">Play the game!</button>
+		</h1>
+	</center>
+</template>
+
+<style scoped>
+
+</style>
