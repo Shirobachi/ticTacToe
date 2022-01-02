604c634ecd76dad42cf753b6b9354d3065717496
Simon Hryszko <simon@hryszko.dev>
2022-01-02 13:12:23 +0100

    Fix src of imgs

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,4 +1,15 @@
+<script setup>
 
+	const img = [
+		new URL('../img/0.png', import.meta.url).href,
+		new URL('../img/1.png', import.meta.url).href
+	]
+
+//code to copy
+	// timer like chess
+
+
+</script>
 
 
 <template>
@@ -6,35 +17,35 @@
 
 		<div class="text-4xl flex justify-center">
 			<div>
-				<img class="p-4" src="1.png" alt="">
+				<img class="p-4" :src="img[Math.floor(Math.random() * 2)]" alt="">
 			</div>
 			<div class="border-l-2 border-r-2">
-				<img class="p-4" src="0.png" alt="">
+				<img class="p-4" :src="img[Math.floor(Math.random() * 2)]" alt="">
 			</div>
 			<div>
-				<img class="p-4" src="1.png" alt="">
+				<img class="p-4" :src="img[Math.floor(Math.random() * 2)]" alt="">
 			</div>
 		</div>
 		<div class="text-4xl flex justify-center">
 			<div class="border-t-2 border-b-2">
-				<img class="p-4" src="1.png" alt="">
+				<img class="p-4" :src="img[Math.floor(Math.random() * 2)]" alt="">
 			</div>
 			<div class="border-t-2 border-b-2 border-l-2 border-r-2">
-				<img class="p-4" src="1.png" alt="">
+				<img class="p-4" :src="img[Math.floor(Math.random() * 2)]" alt="">
 			</div>
 			<div class="border-t-2 border-b-2">
-				<img class="p-4" src="0.png" alt="">
+				<img class="p-4" :src="img[Math.floor(Math.random() * 2)]" alt="">
 			</div>
 		</div>
 		<div class="text-4xl flex justify-center">
 			<div>
-				<img class="p-4" src="1.png" alt="">
+				<img class="p-4" :src="img[Math.floor(Math.random() * 2)]" alt="">
 			</div>
 			<div class="border-l-2 border-r-2">
-				<img class="p-4" src="0.png" alt="">
+				<img class="p-4" :src="img[Math.floor(Math.random() * 2)]" alt="">
 			</div>
 			<div>
-				<img class="p-4" src="1.png" alt="">
+				<img class="p-4" :src="img[Math.floor(Math.random() * 2)]" alt="">
 			</div>
 		</div>
 
