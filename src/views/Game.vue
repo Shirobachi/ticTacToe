7cc81bed25a6a4fd1345553b0a0591fbd1b1eb99
Simon Hryszko <simon@hryszko.dev>
2022-01-02 13:12:03 +0100

    Added game window

---
+++ src/views/Game.vue
@@ -0,0 +1,42 @@
+
+
+
+<template>
+	<main class="container">
+
+		<div class="text-4xl flex justify-center">
+			<div>
+				<img class="p-4" src="1.png" alt="">
+			</div>
+			<div class="border-l-2 border-r-2">
+				<img class="p-4" src="0.png" alt="">
+			</div>
+			<div>
+				<img class="p-4" src="1.png" alt="">
+			</div>
+		</div>
+		<div class="text-4xl flex justify-center">
+			<div class="border-t-2 border-b-2">
+				<img class="p-4" src="1.png" alt="">
+			</div>
+			<div class="border-t-2 border-b-2 border-l-2 border-r-2">
+				<img class="p-4" src="1.png" alt="">
+			</div>
+			<div class="border-t-2 border-b-2">
+				<img class="p-4" src="0.png" alt="">
+			</div>
+		</div>
+		<div class="text-4xl flex justify-center">
+			<div>
+				<img class="p-4" src="1.png" alt="">
+			</div>
+			<div class="border-l-2 border-r-2">
+				<img class="p-4" src="0.png" alt="">
+			</div>
+			<div>
+				<img class="p-4" src="1.png" alt="">
+			</div>
+		</div>
+
+	</main>
+</template>
