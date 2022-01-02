31ba9c1b70053dccfeaf3e82eda898244df05704
Simon Hryszko <simon@hryszko.dev>
2022-01-02 11:40:12 +0100

    Login responsible to change window joining/make new -> real game

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,22 @@
 <script setup>
+import { computed, ref } from "vue";
+import PreGame from "./PreGame.vue";
+import Game from "./Game.vue";
 
-	import PreGame from './PreGame.vue'
+const gameCode = ref();
 
+const setCode = (e) => {
+  gameCode.value = e;
+};
 </script>
 
 <template>
   <div>
     <center>
       <h1>Home</h1>
-				<PreGame></PreGame>
+      <PreGame v-if="gameCode == undefined" @sendCode="setCode"/>
+			<Game v-if="gameCode != undefined" />
+
     </center>
   </div>
 </template>
