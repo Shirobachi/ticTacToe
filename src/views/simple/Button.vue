80287b4c6419cdeef286cbe5b59c3814e8cca342
Simon Hryszko <simon@hryszko.dev>
2022-01-08 17:22:38 +0100

    made button component

---
+++ src/views/simple/Button.vue
@@ -0,0 +1,26 @@
+<script setup>
+	const props = defineProps({
+		disabled: Boolean
+	})
+</script>
+
+<template>
+	
+	<button :disabled="props.disabled" class="
+		bg-primary-color
+		hover:opacity-75
+		text-white
+		px-5
+		py-4
+		text-sm
+		md:text-lg
+		rounded-md
+		font-semibold
+		w-full
+		mx-1
+		disabled:opacity-50
+		">
+		<slot>Button has not content!</slot>
+	</button>
+
+</template>
