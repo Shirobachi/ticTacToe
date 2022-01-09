147d51fe8f555a218b1094cf1cbc2ebaf3262891
Simon Hryszko <simon@hryszko.dev>
2022-01-09 16:28:24 +0100

    Updated about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,14 +1,31 @@
 
 <template>
+	<div class="flex items-center justify-center">
+		<div class="w-full m-4 md:m-0 md:w-1/2 xl:w-3/4">
+			<h1 class="text-xl my-2 font-bold underline">Tic Tac Toe</h1>
+			<span>
+				TicTacToe is a game for two players. At the begging I wrote API for different project, but since I wanted write something amazing I decide to expand API usage to little thing. Like you it went just amazing (If you think different just don't tell me this lied 😛)
+			</span>
+			
+			<h1 class="text-xl my-2 font-bold underline">Usage</h1>
+			<span>
+				This project use API wrote in python's framework fastapi, with units tests wrote in pytest as backend.
+				<br>
+				Frontend is written in Vue.js with some components and some custom components, also tailwind is on the board 💚.
+			</span>
 
-	<div>
-		<center>
-			<h1>About</h1>
-		</center>
+			<h1 class="text-xl my-2 font-bold underline">Feedback</h1>
+			<span>
+				If you have any questions or suggestions please contact me on <a href="https://hryszko.dev" target="_blank" class="text-blue-500 hover:underline hover:text-blue-600">hryszko.dev</a>
+			</span>
 
-		<p>
-			<strong>Tic Tac Toe</strong> is a game where you with your friend(s) play against each other. One person will read question and other player will answer. Each question is like "say 3 thing ...". If player answer correctly, they will get a point otherwise will not. The winner is the one who has the most points.
-	
-		</p>
+			<h1 class="text-xl my-2 font-bold underline">Thanks</h1>
+			<span>
+				Big thanks for all my friends who helped me a lot and crossed their finger for it🤞 Also big thanks for community and all of you who support me.
+				<br>
+				I hope you will enjoy this project. 
+			</span>
+
+		</div>
 	</div>
 </template>
